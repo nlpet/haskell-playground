4804c9d04bfa8925aee0969d3132e5cd55e2e10a
Nora Petrova <schottkey@gmail.com>
2015-08-28 17:06:20 +0100

    Update higherOrderFunctions.hs

--- higherOrderFunctions.hsproj/higherOrderFunctions.hs
+++ higherOrderFunctions.hsproj/higherOrderFunctions.hs
@@ -1,2 +1,9 @@
+-- curried
 compareWithHundred :: (Num a, Ord a) => a -> Ordering
 compareWithHundred = compare 100  
+
+divideByTen :: (Floating a) => a -> a  
+divideByTen = (/10)  
+
+isUpperAlphanum :: Char -> Bool  
+isUpperAlphanum = (`elem` ['A'..'Z'])  
