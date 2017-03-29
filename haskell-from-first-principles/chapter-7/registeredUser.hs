6c0504c81cc35949f13f8652ea71a82fbbf9293c
Nora Petrova <schottkey@gmail.com>
2017-03-30 00:31:35 +0100

    feat: Added a few more exercises from ch.7

--- haskell-from-first-principles/chapter-7/registeredUser.hs
+++ haskell-from-first-principles/chapter-7/registeredUser.hs
@@ -19,3 +19,63 @@ printUser (RegisteredUser (Username name)
 
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
 f (a, b) (c, d) = ((b, d), (a, c))
+
+
+data Employee = Coder
+              | Manager
+              | Veep
+              | CEO
+              deriving (Eq, Ord, Show)
+
+reportBoss :: Employee -> Employee -> IO ()
+reportBoss e e' =
+  putStrLn $ show e ++ " is the bos of " ++ show e'
+
+
+employeeRank :: (Employee -> Employee -> Ordering)
+              -> Employee
+              -> Employee
+              -> IO ()
+
+employeeRank f e e' =
+  case f e e' of
+    GT -> reportBoss e e'
+    EQ -> putStrLn "Neither employee is the boss"
+    LT -> (flip reportBoss) e e'
+
+
+-- Guards
+myAbs :: Integer -> Integer
+myAbs x
+  | x < 0 = (-x)
+  | otherwise = x
+
+
+pythagoreanTriangle :: (Num a, Eq a) => a -> a -> a -> String
+pythagoreanTriangle a b c
+  | a^2 + b^2 == c^2 = "Yes"
+  | otherwise        = "No"
+
+
+dogYears :: Integer -> Integer
+dogYears x
+  | x <= 0    = 0
+  | x <= 1    = x * 15
+  | x <= 2    = x * 12
+  | x <= 4    = x * 8
+  | otherwise = x * 6
+
+
+
+avgGrade :: (Fractional a, Ord a) => a -> Char
+avgGrade x
+  | y >= 0.9   = 'A'
+  | y >= 0.8   = 'B'
+  | y >= 0.7   = 'C'
+  | y >= 0.59  = 'D'
+  | y <  0.59  = 'F'
+  where y = x / 100
+
+
+-- Functional composition
+-- TODO
